929d1ce68762a72e2c0e0ad919fc26b91649ebbd
wangyue <blue_689@163.com>
2018-05-03 02:36:47 -0400

    test marker

--- AR.js-master/aframe/src/component-anchor.js
+++ AR.js-master/aframe/src/component-anchor.js
@@ -97,11 +97,7 @@ AFRAME.registerComponent('arjs-anchor', {
 				markerParameters.type = _this.data.type
 				markerParameters.patternUrl = _this.data.patternUrl;
 				markerParameters.markersAreaEnabled = false
-			}else if( _this.data.preset === 'custom' ){
-+				arProfile.defaultMarkerParameters.type = 'pattern'
-+				arProfile.defaultMarkerParameters.patternUrl = _this.data.patternUrl;
-+				arProfile.defaultMarkerParameters.markersAreaEnabled = false
- 			}else {
+			}else {
 				// console.assert( this.data.preset === '', 'illegal preset value '+this.data.preset)
 			}
 
